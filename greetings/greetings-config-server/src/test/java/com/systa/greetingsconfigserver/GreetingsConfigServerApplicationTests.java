1534a21cc887a83d7c8e8ba77dedc5d229268018
Mohsin0568 <mohsin68.murtuza@gmail.com>
2020-12-01 19:15:41 +0530

    initial commit

---
+++ greetings/greetings-config-server/src/test/java/com/systa/greetingsconfigserver/GreetingsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systa.greetingsconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
